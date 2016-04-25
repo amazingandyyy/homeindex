2179961cf06a542cc8ab1f4113aef27b894bb0d8
amazingandyyy <amazingandyyy@gmail.com>
2016-04-24 18:03:58 -0700

    change resource to https

--- routes/message.js
+++ routes/message.js
@@ -2,25 +2,24 @@
 
 var express = require('express');
 var router = express.Router();
-
 var Message = require('../models/message');
 
 router.post('/', (req, res) => {
-    console.log('ok: ', req.body);
+    console.log('add: ', req.body);
     Message.create(req.body, (err, message) => {
         if (err) return res.status(404).send(err);
         res.status(200).send(message);
     });
 });
 router.delete('/', (req, res) => {
-    console.log(req.body);
+    console.log('delete: ', req.body);
     Message.delete(req.body, (err, message) => {
         if (err) return res.status(404).send(err);
         res.status(200).send(message);
     });
 });
 router.put('/', (req, res) => {
-    console.log(req.body);
+    console.log('update: ', req.body);
     Message.update(req.body, (err, message) => {
         if (err) return res.status(404).send(err);
         res.send('update one successfully');
